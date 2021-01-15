56cb14b2ee75ac1616228c88130d0eda18ac2b8b
georgejob <45026762+georgejob@users.noreply.github.com>
2021-01-15 16:14:40 -0500

    Add files via upload

--- gymbot.py
+++ gymbot.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
+
 class GymBot():
     def __init__(self):
         options = Options()
         options.headless = True
-        self.driver = webdriver.Chrome(chrome_options = options, executable_path = '/home/georgejob6/GymBot/chromedriver')
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path='/home/georgejob6/GymBot/chromedriver')
 
     def login(self):
         self.driver.get('https://services.rec.rutgers.edu/Program/GetProgramDetails?courseId=694b3079-a4f9-4988-94d5-6374f9d3fe40&semesterId=61d7fedd-4078-466e-afda-7003ce3123ac')
@@ -19,19 +20,37 @@ class GymBot():
         sleep(1)
 
         netIDUsername = self.driver.find_element_by_xpath('//*[@id="username"]')
-        netIDUsername.send_keys('XXX')
+        netIDUsername.send_keys('*****')
 
         netIDpw = self.driver.find_element_by_xpath('//*[@id="password"]')
-        netIDpw.send_keys('XXX')
+        netIDpw.send_keys('*****')
 
-        netIDLogin = self.driver.find_element_by_xpath('//*[@id="fm1"]/fieldset/div[5]/input[5]')
+        netIDLogin = self.driver.find_element_by_xpath('//*[@id="fm1"]/section/input[4]')
         netIDLogin.click()
 
     def register(self):
-        #11 am slot, 5 days from now ~ //*[@id="mainContent"]/div[2]/div[7]/div[39]/div/div/div/button
-        registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[1]/div/div/div/button')
+        # 11 am slot, 5 days from now ~ //*[@id="mainContent"]/div[2]/div[7]/div[39]/div/div/div/button //*[@id="mainContent"]/div[2]/div[7]/div[33]/div/div/div/button
+
+        try:
+            registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[49]/div/div/div/button')
+            print("Registered for 49th")
+        except:
+            try:
+                registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[43]/div/div/div/button')
+                print("Registered for 43rd")
+            except:
+                try:
+                    registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[37]/div/div/div/button')
+                    print("Registered for 37th")
+                except:
+                    try:
+                        registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[35]/div/div/div/button')
+                        print("Registered for 35th")
+                    except:
+                        registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[33]/div/div/div/button')
+
         registerButton.click()
-        sleep(1)
+        sleep(2)
 
         q1 = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/form[2]/div[1]/div[1]/div/div[2]/div/div/label[1]')
         q2 = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/form[2]/div[1]/div[2]/div/div[2]/div/div/label[1]')
@@ -49,7 +68,7 @@ class GymBot():
 
         addToCartButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/form[2]/div[2]/button[2]')
         addToCartButton.click()
-    
+
     def checkout(self):
         acknowledgeCookiesButton = self.driver.find_element_by_xpath('//*[@id="gdpr-cookie-accept"]')
         acknowledgeCookiesButton.click()
