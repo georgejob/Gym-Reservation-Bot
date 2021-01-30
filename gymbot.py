4261091cea6f561d34d59ab4ec5816d4fc4f9a1e
georgejob <45026762+georgejob@users.noreply.github.com>
2021-01-30 16:36:56 -0500

    Add files via upload

--- gymbot.py
+++ gymbot.py
@@ -4,11 +4,17 @@ from time import sleep
 
 
 class GymBot():
+
+    # initialize an instance of Chrome for the webdriver to work with
+    # operating in headless mode 
     def __init__(self):
         options = Options()
         options.headless = True
         self.driver = webdriver.Chrome(chrome_options=options, executable_path='/home/georgejob6/GymBot/chromedriver')
 
+    # logs into the website using the top right login button
+    # locates username and password field and fills in corresponding values
+    # locates login button and clicks to login
     def login(self):
         self.driver.get('https://services.rec.rutgers.edu/Program/GetProgramDetails?courseId=694b3079-a4f9-4988-94d5-6374f9d3fe40&semesterId=61d7fedd-4078-466e-afda-7003ce3123ac')
         loginButton = self.driver.find_element_by_xpath('//*[@id="loginLink"]')
@@ -28,26 +34,26 @@ class GymBot():
         netIDLogin = self.driver.find_element_by_xpath('//*[@id="fm1"]/section/input[4]')
         netIDLogin.click()
 
+    # Selects the 11AM - 12:30PM time slot 5 days from the current date to register for
+    # Some time slots five days from now will have different xpaths due to different number of time slots offered on weekends
+    # Answers the 6 question questionaire on the following page and then adds to cart
     def register(self):
-        # 11 am slot, 5 days from now ~ //*[@id="mainContent"]/div[2]/div[7]/div[39]/div/div/div/button //*[@id="mainContent"]/div[2]/div[7]/div[33]/div/div/div/button
+        # 11 am slot, 5 days from now ~ 59th card(Sat), 51st card(Fri,Sun), 43rd card(Mon,Tues,Wed,Thurs)
 
         try:
-            registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[51]/div/div/div/button')
-            print("Registering for 49th card")
+            registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[59]/div/div/div/button')
+            print("Registering for 59th card")
         except:
             try:
-                registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[43]/div/div/div/button')
-                print("Registering for 43rd card")
+                registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[51]/div/div/div/button')
+                print("Registering for 51st card")
             except:
                 try:
-                    registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[37]/div/div/div/button')
-                    print("Registering for 37th card")
+                    registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[43]/div/div/div/button')
+                    print("Registering for 43rd card")
                 except:
-                    try:
-                        registerButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[7]/div[35]/div/div/div/button')
-                        print("Registering for 35th card")
-                    except:
-                        print("Unable to find card...")
+                    print("Unable to find card...")
+                            
 
         registerButton.click()
         sleep(2)
@@ -69,6 +75,7 @@ class GymBot():
         addToCartButton = self.driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/form[2]/div[2]/button[2]')
         addToCartButton.click()
 
+    # Confirms the registration on the final checkout page
     def checkout(self):
         acknowledgeCookiesButton = self.driver.find_element_by_xpath('//*[@id="gdpr-cookie-accept"]')
         acknowledgeCookiesButton.click()
